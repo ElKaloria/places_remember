41198713b722afac8ad3715d691d0e01dacabff5
ElKaloria <maks.dektyarev@yandex.ru>
2024-02-14 17:17:30 +0700

    Change docker-compose.yml file

--- PlacesRemember/PlacesRemember/settings.py
+++ PlacesRemember/PlacesRemember/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'PlacesRemember.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'PlacesRememberdb',
         'USER': 'postgres',
         'PASSWORD': 'maks9877',
